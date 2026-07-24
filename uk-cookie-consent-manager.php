5572edcd411b34caf7073c199c3bbd275ff52d2b
rushleighconsulting <jeremy@rushleighconsulting.co.uk>
2026-07-24 10:56:54 +0100

    Align RC.8 runtime version

--- uk-cookie-consent-manager.php
+++ uk-cookie-consent-manager.php
@@ -17,7 +17,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'UCCM_VERSION', '0.1.0-rc.7' );
+define( 'UCCM_VERSION', '0.1.0-rc.8' );
 define( 'UCCM_PLUGIN_FILE', __FILE__ );
 define( 'UCCM_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'UCCM_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
